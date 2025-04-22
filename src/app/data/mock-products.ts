c804e6c1f4480f6cba40d5ff991bc36158127d8f
moradoulhaj <moradoulhaj.off@gmail.com>
2025-04-22 18:28:22 +0100

    catalogue and show details are ok

--- src/app/data/mock-products.ts
+++ src/app/data/mock-products.ts
@@ -2,12 +2,57 @@ import { Product } from '../models/product';
 
 export const mockProducts: Product[] = [];
 
-for (let i = 1; i <= 50; i++) {
-  const p = new Product(`PROD-${i.toString().padStart(3, '0')}`);
-  p.id = i;
-  p.productTitle = `Product #${i}`;
-  p.productPrice = (Math.random() * 100 + 10).toFixed(2) + ' €';
-  p.productQuantity = Math.floor(Math.random() * 100) + 1;
+const appleProductsData = [
+    {
+      id: 1,
+      productID: 'APL-001',
+      title: 'iPhone 15 Pro Max',
+      price: '1349.00 €',
+      quantity: 25,
+      picture: 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-pro-max-1.jpg',
+    },
+    {
+      id: 2,
+      productID: 'APL-002',
+      title: 'MacBook Air M2',
+      price: '1249.00 €',
+      quantity: 18,
+      picture: 'https://m.media-amazon.com/images/I/71f5Eu5lJSL._AC_SL1500_.jpg',
+    },
+    {
+      id: 3,
+      productID: 'APL-003',
+      title: 'iPad Pro 12.9"',
+      price: '1469.00 €',
+      quantity: 12,
+      picture: 'https://m.media-amazon.com/images/I/81gC7frRJyL._AC_SL1500_.jpg',
+    },
+    {
+      id: 4,
+      productID: 'APL-004',
+      title: 'Apple Watch Series 9',
+      price: '449.00 €',
+      quantity: 30,
+      picture: 'https://m.media-amazon.com/images/I/71T5NVOgbpL._AC_SL1500_.jpg',
+    },
+    {
+      id: 5,
+      productID: 'APL-005',
+      title: 'AirPods Pro (2nd Gen)',
+      price: '299.00 €',
+      quantity: 40,
+      picture: 'https://m.media-amazon.com/images/I/61f1YfTkTDL._AC_SL1500_.jpg',
+    },
+  ];
+  
 
-  mockProducts.push(p);
+for (const data of appleProductsData) {
+  const product = new Product(data.productID);
+  product.id = data.id;
+  product.productTitle = data.title;
+  product.productPrice = data.price;
+  product.productQuantity = data.quantity;
+  product.productPicture = data.picture;
+
+  mockProducts.push(product);
 }
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,48 +1,54 @@
 export class Product {
-  private _id!: number;
-  private _productTitle!: string;
-  private _productPrice!: string;
-  private _productQuantity!: number;
-
-  constructor(readonly productID: string) {}
-
-  // ID
-  public get id(): number {
-    return this._id;
+    private _id!: number;
+    private _productTitle!: string;
+    private _productPrice!: string;
+    private _productQuantity!: number;
+    private _productPicture!: string;
+  
+    constructor(readonly productID: string) {}
+  
+    // ID
+    public get id(): number {
+      return this._id;
+    }
+    public set id(value: number) {
+      this._id = value;
+    }
+  
+    // Title
+    public get productTitle(): string {
+      return this._productTitle;
+    }
+    public set productTitle(value: string) {
+      this._productTitle = value;
+    }
+  
+    // Price
+    public get productPrice(): string {
+      return this._productPrice;
+    }
+    public set productPrice(value: string) {
+      this._productPrice = value;
+    }
+  
+    // Quantity
+    public get productQuantity(): number {
+      return this._productQuantity;
+    }
+    public set productQuantity(value: number) {
+      this._productQuantity = value;
+    }
+  
+    // Picture
+    public get productPicture(): string {
+      return this._productPicture;
+    }
+    public set productPicture(value: string) {
+      this._productPicture = value;
+    }
+  
+    public printProduct(): string {
+      return `id: ${this.id}, productID: ${this.productID}, productTitle: ${this.productTitle}, productPrice: ${this.productPrice}, productQuantity: ${this.productQuantity}, productPicture: ${this.productPicture}`;
+    }
   }
-
-  public set id(value: number) {
-    this._id = value;
-  }
-
-  // Title
-  public get productTitle(): string {
-    return this._productTitle;
-  }
-
-  public set productTitle(value: string) {
-    this._productTitle = value;
-  }
-
-  // Price
-  public get productPrice(): string {
-    return this._productPrice;
-  }
-
-  public set productPrice(value: string) {
-    this._productPrice = value;
-  }
-
-  // Quantity
-  public get productQuantity(): number {
-    return this._productQuantity;
-  }
-
-  public set productQuantity(value: number) {
-    this._productQuantity = value;
-  }
-
-  public printProduct(): string {
-    return `id: ${this.id}, productID: ${this.productID}, productTitle: ${this.productTitle}, productPrice: ${this.productPrice}, productQuantity: ${this.productQuantity}`;
-  }
-}
+  
