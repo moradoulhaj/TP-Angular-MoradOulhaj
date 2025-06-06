0fe51b5f7ed07c0aaed230f038d0eab925797b80
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-06 21:20:54 +0100

    workin on home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
   {path:'authenticate', component:AuthComponent},
+  {path:"" , component:HomeComponent}
 
 
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeroBannerComponent } from "./hero-banner/hero-banner.component";
+import { CategoriesNavComponent } from "./categories-nav/categories-nav.component";
+import { FeaturedCarouselComponent } from "./featured-carousel/featured-carousel.component";
+import { BestDealsComponent } from "./best-deals/best-deals.component";
+import { NewArrivalsComponent } from "./new-arrivals/new-arrivals.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroBannerComponent, CategoriesNavComponent, FeaturedCarouselComponent, BestDealsComponent, NewArrivalsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
