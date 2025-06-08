a08716ac6efd5d0346bc74cc4b01da71adf4bee4
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-09 00:00:07 +0100

    the login and register components protected succesfully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './components/common/product-details-modal/product-details-modal.component';
 import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
+import { UnauthGuard } from './guards/unauth.guard';
 
 export const routes: Routes = [
-  { path: 'auth', component: AuthComponent },
+  { path: 'auth', component: AuthComponent,canActivate:[UnauthGuard] },
   { path: '', component: LandingPageComponent },
   { path: 'home', component: HomeComponent },
 
