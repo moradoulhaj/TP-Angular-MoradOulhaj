aa98e79cb6685e393a0f6a1ec2738a4cbda9d807
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-12 00:29:02 +0100

    header done and fixing all the problems

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { CartService } from '../../services/cart.service';
 import { Cart } from '../../models/cart';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-header',
@@ -16,6 +17,7 @@ import { Cart } from '../../models/cart';
 export class HeaderComponent implements OnInit, OnDestroy {
   cart: Cart[] = []; // Initialize cart as an empty array
   isLoggedIn = false;
+  user: User | null = null; // Holds the current user information, initialized to null
   private subscription?: Subscription;
   cartTotal: number = 0; // Holds the total cost of the cart
 
@@ -30,13 +32,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
       this.isLoggedIn = status;
     });
     if (this.isLoggedIn) {
-
+      this.user = JSON.parse(localStorage.getItem('user') || 'null'); // Retrieve user info from localStorage
       this.cartService.getCart().subscribe({
         next: (cart) => {
           this.cart = cart; // Update the cart service with the fetched cart
           this.cartTotal = this.cart.reduce((total, item) => {
             return total + item.priceProduct * item.count;
-          }, 0);          
+          }, 0);
         },
         error: (error) => {
           console.error('Error fetching cart:', error);
@@ -60,6 +62,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
       case 'landingpage':
         this.router.navigate(['/']);
         break;
+      case 'products':
+        this.router.navigate(['/products']);
+        break;
+        case 'home':
+          this.router.navigate(['/']);
+          break;
       default:
         console.warn('Unknown destination:', destination);
         break;
