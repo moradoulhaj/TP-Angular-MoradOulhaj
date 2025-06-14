65bb76ba7790881aee4b246246c37db218a4e3f1
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-14 22:48:50 +0100

    last working version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
-import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
-import { UnauthGuard } from './guards/unauth.guard';
-import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './components/common/product-details-modal/product-details-modal.component';
+import { CartComponent } from './components/cart/cart.component';
 
 export const routes: Routes = [
-  { path: 'auth', component: AuthComponent, canActivate: [UnauthGuard] },
-  { path: '', component: LandingPageComponent },
-  { path: 'products', component: ProductListingComponent },
+  { path: 'authenticate', component: AuthComponent },
+  { path: '', component: HomeComponent },
+  { path: '/cart', component: CartComponent },
+
+
+  { path: 'product/:id', component: ProductDetailsComponent },
+
 ];
