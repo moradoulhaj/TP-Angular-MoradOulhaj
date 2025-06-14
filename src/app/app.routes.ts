2336de51aff35b96da0afbd6e3259716f519eeaf
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-14 22:49:28 +0100

    last workin version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
-import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './components/common/product-details-modal/product-details-modal.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailsModalComponent } from './components/common/product-details-modal/product-details-modal.component';
+import { ProductListingComponent } from './components/product-listing/product-listing.component';
 
 export const routes: Routes = [
   { path: 'authenticate', component: AuthComponent },
-  { path: '', component: HomeComponent },
-  { path: '/cart', component: CartComponent },
+  { path: '', component: ProductListingComponent }, // Assuming HomeComponent is imported and defined elsewhere
+  { path: 'cart', component: CartComponent },
 
 
-  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'product/:id', component: ProductDetailsModalComponent },
 
 ];
